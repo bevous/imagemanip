77833e1f3749f1428298380be4689e5fdf7696dd
bevous <43658573+bevous@users.noreply.github.com>
2018-12-11 23:44:17 -0600

    have fun

---
+++ image magic/image magic/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace image_magic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.picture1 = new System.Windows.Forms.PictureBox();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.OpenFile = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(612, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "apply setting";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(612, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 66);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "save settings";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(703, 372);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 66);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "save image";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // picture1
+            // 
+            this.picture1.Location = new System.Drawing.Point(12, 12);
+            this.picture1.Name = "picture1";
+            this.picture1.Size = new System.Drawing.Size(297, 426);
+            this.picture1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture1.TabIndex = 3;
+            this.picture1.TabStop = false;
+            // 
+            // picture2
+            // 
+            this.picture2.Location = new System.Drawing.Point(306, 12);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(297, 426);
+            this.picture2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture2.TabIndex = 4;
+            this.picture2.TabStop = false;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(678, 12);
+            this.maskedTextBox1.Mask = "00000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 26);
+            this.maskedTextBox1.TabIndex = 5;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(678, 54);
+            this.maskedTextBox2.Mask = "00000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(100, 26);
+            this.maskedTextBox2.TabIndex = 6;
+            this.maskedTextBox2.ValidatingType = typeof(int);
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(678, 95);
+            this.maskedTextBox3.Mask = "00000";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(100, 26);
+            this.maskedTextBox3.TabIndex = 7;
+            this.maskedTextBox3.ValidatingType = typeof(int);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(650, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "R";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(650, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(650, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "B";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 448);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(766, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(703, 316);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 50);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "load image";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.FileName = "openFileDialog1";
+            this.OpenFile.Filter = ".jpeg|.png";
+            this.OpenFile.InitialDirectory = "Pictures";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.picture2);
+            this.Controls.Add(this.picture1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox picture1;
+        private System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog OpenFile;
+    }
+}
+
---
+++ image magic/image magic/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace image_magic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //opens image for edditing
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        private void Label1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "JPG|*.JPG";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Image myImage = Bitmap.FromFile(openFileDialog1.FileName);
+                picture1.Image = myImage;
+                picture2.Image = myImage;
+            }
+        }
+
+
+        //saves the modified image
+        SaveFileDialog SaveFileDialog1 = new SaveFileDialog();
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog1.Filter = "JPG|*.JPG";
+            if (SaveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                picture2.Image.Save(SaveFileDialog1.FileName);
+            }
+        }
+        
+        //apply changes
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            BackgroundWorker worker1 = new BackgroundWorker();
+            worker1.DoWork += (obj,ea) => SortImage();
+            worker1.RunWorkerAsync();
+            
+        }
+        public class ColorCompare : IComparer<Color>
+        {
+            public int Compare(Color x, Color y)
+            {
+                if (x.B.CompareTo(y.B) != 0)
+                {
+                    return x.B.CompareTo(y.B);
+                }
+                else if (x.R.CompareTo(y.R) != 0)
+                {
+                    return x.R.CompareTo(y.R);
+                }
+                else if (x.G.CompareTo(y.G) != 0)
+                {
+                    return x.G.CompareTo(y.G);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        public void SortImage()
+        {
+            //grab the image from box 2
+            Image ModImage = picture2.Image;
+            //save the image into a bitmap for editing
+            Bitmap ImagePix = new Bitmap(ModImage);
+            //set the dimentions
+            int height = ImagePix.Height;
+            int width = ImagePix.Width;
+            //number of times the loop needs to complete
+            List<Color> pixel = new List<Color>();
+            
+
+            
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    
+                    if (x + 1 < width && y + 1 < height)
+                    {
+                        pixel.Add(ImagePix.GetPixel(x, y));
+                    }
+                }
+            }
+            ColorCompare Cc = new ColorCompare();
+            //pixel.Sort(Cc);
+            
+            int L = 0;
+            int a = -1;
+
+
+            List<Color> design = new List<Color>();
+            design.Capacity = pixel.Capacity;
+            foreach(Color bleh in pixel)
+            {
+                design.Add(Color.Beige);
+            }
+            //change this for different looks
+            int counter = 0;
+            int IndexA = 0;
+            //int IndexB = (design.Count - 1) / 2;
+            foreach (Color color in pixel)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = counter; y < height; y++)
+                    {
+
+                    }
+                }
+            }
+            /////////////
+            for (int x = 0; x < pixel.Count; x++)
+            {
+             
+                a++;
+                ImagePix.SetPixel(a, L, design[x]);
+                if ((x % (width - 1)) == 0)
+                {
+                    L++;
+                    a = -1;
+                }
+            }
+
+            
+            //update the image
+            picture2.Image = ImagePix;
+            //update the box2
+
+            Console.WriteLine("done");
+        }
+
+       
+    }
+}
